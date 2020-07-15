71ee57bd1fd21418005e004ad8748d0b5f068cf2
rupeshkalra <42487080+rupeshkalra@users.noreply.github.com>
2020-07-15 16:50:27 +0530

    first commit

---
+++ findmax.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class findmax {
+public static void main(String[] args)
+{
+	Scanner sc =new Scanner(System.in);
+	int n=sc.nextInt();
+	int max=0;
+	int a[]=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		a[i]=sc.nextInt();
+		if(a[i]>max)
+			max=a[i];
+    }
+    sc.close();
+	System.out.print(max);
+}
+}
