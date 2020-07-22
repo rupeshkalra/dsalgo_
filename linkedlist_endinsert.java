19bb94128ba544ef0913afb31c99494f07146fec
rupeshkalra <42487080+rupeshkalra@users.noreply.github.com>
2020-07-22 22:49:27 +0530

    commit

---
+++ linkedlist_endinsert.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class linkedlist_endinsert
+{
+    static class Node
+    {
+    int data;
+    Node next;
+    Node(int d)
+    {
+        data =d;
+        next=null;
+    }
+    }
+    public Node head =null;
+   public void insert(int d)
+   {
+      Node ele =new Node(d);
+      if(head==null)
+      {
+      head =ele;
+      }
+      else{
+          Node last =head;
+          while(last.next!=null)
+          {
+              last=last.next;
+          }
+          last.next=ele;
+      }
+
+   }
+   public void disp()
+   {
+      Node curr =head;
+      if(curr==null)
+      {
+          System.out.print("empty");
+      }
+      else{
+          while(curr !=null)
+          {
+              System.out.print(curr.data+ " ");
+              curr=curr.next;
+          }
+      }
+   }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        linkedlist_endinsert in =new linkedlist_endinsert();
+        for(int i=0;i<n;i++)
+        {
+          int ele=sc.nextInt();
+          in.insert(ele);
+        }
+        sc.close();
+        in.disp();
+    }
+}
