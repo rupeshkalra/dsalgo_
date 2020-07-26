37c594f0c35149b92c5f778d071cdb34acb0345a
rupeshkalra <42487080+rupeshkalra@users.noreply.github.com>
2020-07-26 13:46:33 +0530

    commit

---
+++ linkedlist_findelement.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class linkedlist_findelement
+{   class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data=d;
+            next=null;
+        }
+    }
+    Node head;
+    public void insert(int d)
+    {
+         Node ele=new Node(d);
+         if(head==null)
+         {
+           head=ele;
+         }
+         else{
+            Node last =head;
+            while(last.next!=null)
+            {
+                last=last.next;
+            }
+            last.next=ele;
+         }
+    }
+    public void find(int k)
+    {   int flag=0;
+        Node curr=head;
+        int i=1;
+        while(curr.next!=null)
+        {
+            if(curr.data==k)
+            {   flag=1;
+                break;
+            }
+            i++;
+            curr=curr.next;
+        }
+        if(curr.data==k)
+        flag=1;
+        
+        if(flag==1)
+        {
+            System.out.println("found at position: "+i);
+        }
+        else{
+            System.out.println("not found!!");
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        linkedlist_findelement in =new linkedlist_findelement();
+        for(int i=0;i<n;i++)
+        {
+            int el =sc.nextInt();
+            in.insert(el);
+        }
+        
+        System.out.println("enter element to find:");
+        int k=sc.nextInt();
+        in.find(k);
+        sc.close();
+    }
+}
---
+++ linkedlist_merge.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class linkedlist_merge
+{
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter size of first list:");
+      int n=sc.nextInt();
+      for(int i=0;i<n;i++)
+      {
+        int el=sc.nextInt();
+      }
+      
+    }
+}
